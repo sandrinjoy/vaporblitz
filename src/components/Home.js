9c5cc41b9dd64678edb361d4376f3cbcf1d1d8c7
sandrinejoy <sandrinejoy007@gmail.com>
2021-10-18 21:07:32 +0530

    design changes, image changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React ,{useState,useEffect} from 'react';
 
-import bg from "./../assets/bg.png"
-import tree from "./../assets/tree.png"
+import tree from "./../assets/tree.webp"
 import gun1 from "./../assets/gun1.png"
 import gun2 from "./../assets/gun2.png"
 import gun3 from "./../assets/gun3.png"
@@ -10,15 +9,13 @@ import acc1 from "./../assets/acc1.png"
 import acc2 from "./../assets/acc2.png"
 import acc3 from "./../assets/acc3.png"
 import acc4 from "./../assets/acc4.png"
-
-import boss1 from "./../assets/boss1.png"
-import boss2 from "./../assets/boss2.png"
-import boss3 from "./../assets/boss3.png"
+import boss1 from "./../assets/boss1.webp"
+import boss2 from "./../assets/boss2.webp"
+import boss3 from "./../assets/boss3.webp"
 import fun1 from "./../assets/fun1.png"
-import house from "./../assets/house.png"
-
+import house from "./../assets/house.webp"
 import chart from "./../assets/chart.png"
-import {FaDiscord,FaTelegram} from 'react-icons/fa';
+import {FaDiscord, FaTelegramPlane} from 'react-icons/fa';
 import {AiOutlineDown,AiOutlineCopy} from 'react-icons/ai'
 import brand from "./../assets/brand.png"
 import { Image, Col, Button, Row, Card, Container } from "react-bootstrap";
@@ -35,13 +32,13 @@ export default function Home() {
            </div>
              <Container fluid className="front d-flex  align-items-center justify-content-center  bg-black bg-photo " style={{height: '100vh'}}>
             <div className="front float-right-top d-flex">
-             <a className="btn mx-1 btn-dark bg-accent front rounded-circle border-0" href="#" role="button"><FaDiscord/></a>
-             <a className="btn mx-1 btn-dark bg-accent2 front rounded-circle border-0" href="#" role="button"><FaTelegram/></a>
+             <a className="btn m-2 btn-dark bg-accent front rounded-circle border-0 d-flex justify-content-center align-items-center" href="#" role="button"><FaDiscord size={24} /></a>
+             <a className="btn m-2 btn-dark bg-accent2 front rounded-circle border-0 d-flex justify-content-center align-items-center" href="#" role="button"><FaTelegramPlane size={24}/></a>
             </div>
             <section className="front  mx-auto my-auto d-flex flex-column justify-content-center align-items-center  w-100  ">
               
           
-         <img src={brand} alt="VapoBlitz" className="front img-fluid w-25" />
+         <img src={brand} alt="VapoBlitz" className="front img-fluid img-brand" />
                <hr color="#fff"/>
                    
                <Button variant="dark" className="front bg-accent py-1 my-2 ">Play For Free</Button>
@@ -53,9 +50,9 @@ export default function Home() {
     </Container>
     
     
-    <Container fluid className="  d-flex flex-column py-5 px-1 bg-section1" >
+    <Container fluid className="  d-flex flex-column py-5  bg-section1" >
       <h3 className="text-center text-white fs-2 pb-3">What is Vaporblitz?</h3>
-        <p className="text-center align-self-center text-white fs-5 fw-light w-75  px-1">
+        <p className="text-center align-self-center text-white fs-5 fw-light w-75  ">
         Vaporblitz is a PVP - P2E game where you
 fight against other players online for BNB or VPR. 
 All in-game items are NFTS.
@@ -64,7 +61,7 @@ All in-game items are NFTS.
 to make money in the process.
         </p>
     </Container>
-    <Container fluid className="  d-flex flex-column py-5 px-1 bg-section2" >
+    <Container fluid className="  d-flex flex-column py-5  bg-section2" >
       <h3 className="text-center text-white fs-2 pb-1">Collect NFT Items?</h3>
       
       <h3 className="text-center text-white fs-5 fw-light pb-3">All usable in Game!</h3>
@@ -72,16 +69,16 @@ to make money in the process.
             <Row className="mt-5 text-light">NFT Weapon Skins</Row>
          <Row>
              <Col md={3} className="d-flex justify-content-center align-items-center  ">
-                 <div className=""><img src={gun1} className="img-fluid w-75 p-3  "/></div>
+                 <div className="text-center"><img src={gun1} className="img-fluid w-75 p-3  "/></div>
              </Col>
              <Col md={3} className="d-flex justify-content-center align-items-center ">
-             <div className=""><img src={gun2} className="img-fluid w-75 p-3  "/></div>
+             <div className="text-center"><img src={gun2} className="img-fluid w-75 p-3  "/></div>
          </Col>
              <Col md={3} className="d-flex justify-content-center align-items-center ">
-             <div className=""><img src={gun3} className="img-fluid w-75 p-3  "/></div>
+             <div className="text-center"><img src={gun3} className="img-fluid w-75 p-3  "/></div>
          </Col>
              <Col md={3} className="d-flex justify-content-center align-items-center ">
-             <div className=""><img src={gun4} className="img-fluid w-75 p-3  "/></div>
+             <div className="text-center"><img src={gun4} className="img-fluid w-75 p-3  "/></div>
          </Col>
          
 
@@ -90,16 +87,16 @@ to make money in the process.
          <Row className=" mt-5 text-light">NFT Accessories</Row>
          <Row>
              <Col md={3} className="d-flex justify-content-center align-items-center  ">
-                 <div className=""><img src={acc1} className="img-fluid w-75 p-3  "/></div>
+                 <div className="text-center"><img src={acc1} className="img-fluid w-75 p-3  "/></div>
              </Col>
              <Col md={3} className="d-flex justify-content-center align-items-center ">
-             <div className=""><img src={acc2} className="img-fluid w-75 p-3  "/></div>
+             <div className="text-center"><img src={acc2} className="img-fluid w-75 p-3  "/></div>
          </Col>
              <Col md={3} className="d-flex justify-content-center align-items-center ">
-             <div className=""><img src={acc3} className="img-fluid w-75 p-3  "/></div>
+             <div className="text-center"><img src={acc3} className="img-fluid w-75 p-3  "/></div>
          </Col>
              <Col md={3} className="d-flex justify-content-center align-items-center ">
-             <div className=""><img src={acc4} className="img-fluid w-75 p-3  "/></div>
+             <div className="text-center"><img src={acc4} className="img-fluid w-75 p-3  "/></div>
          </Col>
          
 
@@ -108,26 +105,26 @@ to make money in the process.
         </Container>
        
     </Container>
-    <Container fluid className="  d-flex flex-column py-5 px-1 bg-section1" >
+    <Container fluid className="  d-flex flex-column py-5  bg-section1" >
       <h3 className="text-center text-white fs-2 pb-3">Complete PVP System</h3>
-        <p className="text-center align-self-center text-white fs-5 fw-light w-75  px-1">
+        <p className="text-center align-self-center text-white fs-5 fw-light w-75  ">
         Fun and Fast-Paced Gameplay with different maps and weapons to choose from.
         </p>
         <Row>
-             <Col md={4} className="d-flex justify-content-center align-items-center  ">
-             <div className="d-flex flex-column justify-content-center align-items-center"><img src={fun1} className="img-fluid w-75 p-3  "/>
+             <Col md={4} className="m-auto  ">
+             <div className="m-auto text-center"><img src={fun1} className="img-fluid w-50 p-3  "/>
              <h4 className="text-white">Fun Weapons</h4>
              <p className="text-light">Over 10 weapons with different abilities</p>
              
              </div>
          </Col>
-             <Col md={4} className="d-flex justify-content-center align-items-center ">
-             <div className="d-flex flex-column justify-content-center align-items-center"><img src={fun1} className="img-fluid w-75 p-3  "/>
+             <Col md={4} className="m-auto ">
+             <div className="m-auto text-center"><img src={fun1} className="img-fluid w-50 p-3  "/>
              <h4 className="text-white">Fun Weapons</h4>
              <p className="text-light">Over 10 weapons with different abilities</p></div>
           </Col>
-             <Col md={4} className="d-flex justify-content-center align-items-center ">
-             <div className="d-flex flex-column justify-content-center align-items-center"><img src={fun1} className="img-fluid w-75 p-3  "/>
+             <Col md={4} className="m-auto ">
+             <div className="m-auto text-center"><img src={fun1} className="img-fluid w-50 p-3  "/>
              <h4 className="text-white">Fun Weapons</h4>
              <p className="text-light">Over 10 weapons with different abilities</p></div>
          </Col>
@@ -136,27 +133,27 @@ to make money in the process.
 
 
          </Row>
-         <Row><h4 className="text-white text-center mt-5">Fight On Different Maps</h4>
-           </Row>
+        <h4 className="text-white text-center mt-5">Fight On Different Maps</h4>
+         
     </Container>
-    <Container fluid className="  d-flex flex-column py-5 px-1 bg-section2" >
+    <Container fluid className="  d-flex flex-column py-5  bg-section2" >
       <h3 className="text-center text-white fs-2 pb-3">Fight Bosses for VPR</h3>
-        <p className="text-center align-self-center text-white fs-5 fw-light w-75  px-1">
+        <p className="text-center align-self-center text-white fs-5 fw-light w-75  ">
         Fight Bosses in P2E and win VPR.</p>
         
         <Row>
-             <Col md={4} className="d-flex justify-content-center align-items-center  ">
-             <div className="d-flex flex-column justify-content-center align-items-center"><img src={boss1} className="img-fluid w-75 p-3  "/>
+             <Col md={4} className="m-auto ">
+             <div className="m-auto text-center"><img src={boss1} className="img-fluid w-75 p-3  "/>
              <h4 className="text-white">Signal Killer</h4>
              
              </div>
          </Col>
-             <Col md={4} className="d-flex justify-content-center align-items-center ">
-             <div className="d-flex flex-column justify-content-center align-items-center"><img src={boss2} className="img-fluid w-75 p-3  "/>
+             <Col md={4} className="m-auto">
+             <div className="m-auto text-center"><img src={boss2} className="img-fluid w-75 p-3  "/>
              <h4 className="text-white">Fruit Snatcher</h4></div>
           </Col>
-             <Col md={4} className="d-flex justify-content-center align-items-center ">
-             <div className="d-flex flex-column justify-content-center align-items-center"><img src={boss3} className="img-fluid w-75 p-3  "/>
+             <Col md={4} className="m-auto">
+             <div className="m-auto text-center"><img src={boss3} className="img-fluid w-75 p-3  "/>
              <h4 className="text-white">Dead Dice</h4>
              </div>
           </Col>
@@ -165,40 +162,40 @@ to make money in the process.
 
 
          </Row>
-        <p className="mt-3 text-center align-self-center text-white fs-5 fw-light w-75  px-1">
+        <p className="mt-3 text-center align-self-center text-white fs-5 fw-light w-75  ">
         Fight different bosses in PVE, bosses have different attack styles and
 abilities. After beating a boss you are rewarded with $VPR.</p>
         
     </Container>
-    <Container fluid className="  d-flex flex-column py-5 px-1 bg-section1" >
+    <Container fluid className="  d-flex flex-column py-5  bg-section1" >
       <h3 className="text-center text-white fs-2 pb-3">Own and Customise an NFT Home</h3>
-        <p className="text-center align-self-center text-white fs-5 fw-light w-75  px-1">
+        <p className="text-center align-self-center text-white fs-5 fw-light w-75  ">
         Buy a property, customise it and earn daily VPR from it.</p>
         <img src={house} className="img-fluid w-50 align-self-center"/>
-        <p className="text-center align-self-center text-white fs-5 fw-light w-75  px-1">
+        <p className="text-center align-self-center text-white fs-5 fw-light w-75  ">
         The more expensive the property, the higher the daily rewards</p>
       
        
     </Container>
-    <Container fluid className="  d-flex flex-column py-5 px-1 bg-section2" >
+    <Container fluid className="  d-flex flex-column py-5  bg-section2" >
       <h3 className="text-center text-white fs-2 pb-3">Customise your Character</h3>
-        <p className="text-center align-self-center text-white fs-5 fw-light w-75  px-1">
+        <p className="text-center align-self-center text-white fs-5 fw-light w-75  ">
         Pick from different clothes, accessories and more.</p>
        
        
     </Container>
-    <Container fluid className="  d-flex flex-column py-5 px-1 bg-section1" >
+    <Container fluid className="  d-flex flex-column py-5  bg-section1" >
       <h3 className="text-center text-white fs-2 pb-3">Tokenomics</h3>
-        <p className="text-center align-self-center text-white fs-5 fw-light w-75  px-1">
+        <p className="text-center align-self-center text-white fs-5 fw-light w-75  ">
         The games main token's tokenomics</p>
         <img src={chart} className="img-fluid w-50 align-self-center"/>
      
        
        
     </Container>
-    <Container fluid className="  d-flex flex-column py-5 px-1 bg-section3" >
+    <Container fluid className="  d-flex flex-column py-5  bg-section3" >
       <h3 className="text-center text-white fs-2 pb-3">Buy $VPR</h3>
-        <p className="text-center align-self-center text-white fs-5 fw-light w-75  px-1">
+        <p className="text-center align-self-center text-white fs-5 fw-light w-75  ">
         Buy the games main token on pancakeswap</p>
         <div className="d-flex flex-column w-50 align-self-center">
         <Button variant="dark" className=" bg-accent py-1 my-2 ">Buy on Pancakeswap </Button>
